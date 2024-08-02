4cf0790d6988ec37a142ed1dce77db600e3c053a
GabFigueiredo <gabrielfandradee2@gmail.com>
2024-08-02 15:56:28 -0300

    Last update before new UI

--- client/src/components/delete/confirmationModal.jsx
+++ client/src/components/delete/confirmationModal.jsx
@@ -1,4 +1,4 @@
-import { React, useEffect, useState } from "react";
+import { React, useState } from "react";
 import axios from "axios";
 import styles from './delete.module.css'
 import { styled } from '@mui/material/styles';
